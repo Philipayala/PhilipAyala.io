56a22287e2387bbf78a30c9182e05b5ea0fae13d
Philipayala <63319238+Philipayala@users.noreply.github.com>
2020-04-20 22:00:15 +0200

    Add files via upload

---
+++ js/slider.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+  $(".center-slider").slick({
+
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots:true,
+    fade: true,
+    arrows: false,
+    infinite: true,           
+    autoplay: true,
+    autoplayspeed: 2000
+    
+   });
+  })
+
+
